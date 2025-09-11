f7fee009525ded07dacb668bfa63eb378787cda3
ssjssammm <sam.yomijohnson@mail.utoronto.ca>
2025-09-11 12:20:05 -0400

    Adding Multiples file and creating its main method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -6,7 +6,7 @@ public class Multiples {
        int count = 0;
        for (i=0; i<1000; i++) {
            if (i % 3 == 0 || i % 5 == 0){
-               count ++;
+               count +=1;
            }
        }
        System.out.println(count);
